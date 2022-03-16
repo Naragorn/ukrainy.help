893b67e38837fde3432a569d370bc77312d5ebfb
Nadim Yonis <nyonis@tecracer.de>
2022-03-16 11:42:38 +0000

    Revert "Added terraform state"
    This reverts commit a9d655d7f9ade3d23b3d10688fb2dec17bcddbab.

--- sms/state.tf
+++ sms/state.tf
@@ -1,7 +1,4 @@
-resource "aws_s3_bucket" "terraform_state_bucket" {
   #acl    = "private"
-  bucket = "ukrainy-help-terraform-state"
-
   #   server_side_encryption_configuration {
   #     rule {
   #       apply_server_side_encryption_by_default {
@@ -15,42 +12,3 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
   #     enabled    = true
   #     mfa_delete = false
   #   }
-
-  #tags = merge(var.tags, { Name = var.s3_tf_state_bucket })
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_state_bucket_block" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  depends_on = [
-    aws_s3_bucket.terraform_state_bucket
-  ]
-}
-
-resource "aws_dynamodb_table" "terraform_lock_state" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  server_side_encryption {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
